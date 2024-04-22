53b898835888806a4640cb7ea595ca64b4d0725c
PandaJW <5168pandayoyo@gmail.com>
2024-04-22 14:26:48 +0800

    added certs page placeholder

---
+++ lib/screens/certifications/certs_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/constant.dart';
+
+class CertsPage extends StatelessWidget {
+  const CertsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
