3994da6da0c6cacb6df513910f0918535c6bb6a9
PandaJW <5168pandayoyo@gmail.com>
2024-04-22 14:27:21 +0800

    added certs to navigation

--- lib/screens/main/components/navigation_button_list.dart
+++ lib/screens/main/components/navigation_button_list.dart
@@ -33,13 +33,13 @@ class NavigationButtonList extends StatelessWidget {
                           curve: Curves.easeIn);
                     },
                     text: 'Projects'),
-              // NavigationTextButton(
-              //     onTap: () {
-              //       controller.animateToPage(2,
-              //           duration: const Duration(milliseconds: 500),
-              //           curve: Curves.easeIn);
-              //     },
-              //     text: 'Certifications'),
+              NavigationTextButton(
+                  onTap: () {
+                    controller.animateToPage(2,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeIn);
+                  },
+                  text: 'Certifications'),
               // NavigationTextButton(onTap: () {}, text: 'Achievements'),
             ],
           ),
