9bea2ce86644de9165c26a172a16dd5e949fdd90
PandaJW <5168pandayoyo@gmail.com>
2024-04-26 21:57:23 +0800

    added title in about page

--- lib/screens/about/about_page.dart
+++ lib/screens/about/about_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/view_model/responsive.dart';
+import 'package:flutter_portfolio/constant.dart';
+import 'package:flutter_portfolio/screens/certifications/components/title_text.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          if (Responsive.isLargeMobile(context))
+            const SizedBox(
+              height: defaultPadding,
+            ),
+          const TitleText(prefix: 'About ', title: 'Me'),
+          const SizedBox(
+            height: defaultPadding,
+          ),
+        ],
+      ),
+    );
   }
 }
