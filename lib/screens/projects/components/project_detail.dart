23f7d7436c9b6fcf685aa1460bca515c3e6e35e5
PandaJW <5168pandayoyo@gmail.com>
2025-03-11 16:32:41 +0800

    updated projects to show full description instead of ellipsis

--- lib/screens/projects/components/project_detail.dart
+++ lib/screens/projects/components/project_detail.dart
@@ -7,9 +7,9 @@ import 'package:flutter_portfolio/view_model/responsive.dart';
 class ProjectDetail extends StatelessWidget {
   final int index;
   const ProjectDetail({super.key, required this.index});
+
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.sizeOf(context);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -21,8 +21,6 @@ class ProjectDetail extends StatelessWidget {
                 .textTheme
                 .headlineSmall!
                 .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
           ),
         ),
         Responsive.isMobile(context)
@@ -32,21 +30,15 @@ class ProjectDetail extends StatelessWidget {
             : const SizedBox(
                 height: defaultPadding,
               ),
-        Text(
-          projectList[index].description,
-          style: const TextStyle(color: Colors.grey, height: 1.5),
-          maxLines: size.width > 700 && size.width < 750
-              ? 3
-              : size.width < 470
-                  ? 2
-                  : size.width > 600 && size.width < 700
-                      ? 6
-                      : size.width > 900 && size.width < 1060
-                          ? 6
-                          : 4,
-          overflow: TextOverflow.ellipsis,
+        // Scrollable Container for Description
+        Expanded(
+          child: SingleChildScrollView(
+            child: Text(
+              projectList[index].description,
+              style: const TextStyle(color: Colors.grey, height: 1.5),
+            ),
+          ),
         ),
-        const Spacer(),
         ProjectLinks(
           index: index,
         ),
